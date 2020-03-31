626c757a388bcb4503a7e9427fb5b7488dde875a
Sam Robinson <robinsonsa@mtc.byu.edu>
2020-03-31 15:08:22 -0600

    reading auth state

--- src/app/auth/components/login-page/login-page.component.ts
+++ src/app/auth/components/login-page/login-page.component.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Store } from "@ngrx/store";
-import { State } from "src/app/shared/state";
+import {
+  State,
+  selectGettingAuthStatus,
+  selectAuthUser,
+  selectAuthError
+} from "src/app/shared/state";
 import { UserModel } from "src/app/shared/models";
 import { AuthUserActions } from "../../actions";
 import { LoginEvent } from "../login-form";
@@ -12,14 +17,15 @@ import { LoginEvent } from "../login-form";
   styleUrls: ["./login-page.component.css"]
 })
 export class LoginPageComponent {
-  gettingStatus$: Observable<boolean> = of(false);
-  user$: Observable<UserModel | null> = of({
-    id: "1",
-    username: "NgRx Learner"
-  });
-  error$: Observable<string | null> = of(null);
+  gettingStatus$: Observable<boolean>;
+  user$: Observable<UserModel | null>;
+  error$: Observable<string | null>;
 
-  constructor(private store: Store<State>) {}
+  constructor(private store: Store<State>) {
+    this.gettingStatus$ = this.store.select(selectGettingAuthStatus);
+    this.user$ = this.store.select(selectAuthUser);
+    this.error$ = this.store.select(selectAuthError);
+  }
 
   onLogin($event: LoginEvent) {
     this.store.dispatch(AuthUserActions.login($event.username, $event.password));
